54255371d1465991130573eb07a55ffd0346be39
admin <admin@admins-MacBook-Air.local>
2026-04-28 01:13:23 +0530

    Initial Django Blog Setup

---
+++ blog_main/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+def search(request):
+    return render(request, "search.html")
