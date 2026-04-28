ed3d94d9234a40ae9e9327d130bfcadee4807925
admin <admin@admins-MacBook-Air.local>
2026-04-28 19:44:15 +0530

    blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "blogs",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATICFILES_DIRS = ["blog_main/static"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
